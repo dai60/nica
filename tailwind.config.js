56b951631e1d767e0b6a8af391b93087da2345dd
dai60 <190627676+dai60@users.noreply.github.com>
2024-12-06 13:14:21 +0200

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"],
+                "roboto-condensed": ["'Roboto Condensed'", "sans-serif"],
+            }
+        },
     },
     plugins: [],
 }
