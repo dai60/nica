35301a658ca5084b1abe992e30471e6b563e9578
dai60 <190627676+dai60@users.noreply.github.com>
2024-12-06 16:04:45 +0200

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
+            colors: {
+                primary: "#404445",
+                secondary: "#5F6B81",
+                green: "#43C45F",
+            },
             fontFamily: {
                 roboto: ["Roboto", "sans-serif"],
                 "roboto-condensed": ["'Roboto Condensed'", "sans-serif"],
